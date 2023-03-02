da5cd4f3eb61f225d1566c952cdd56f862db1ee6
Wichitt <36907668+wichitt@users.noreply.github.com>
2023-03-02 19:05:34 +0700

    Update ft_strlen.c

--- ft_strlen.c
+++ ft_strlen.c
@@ -9,7 +9,9 @@
 /*   Updated: 2023/02/27 13:01:19 by wamonvor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+/*
+ check leng of string
+*/
 #include "libft.h"
 
 size_t	ft_strlen(const char *s)
